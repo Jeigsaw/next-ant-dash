8aafc74d4a12cf3b359f6da4426a03efb33bbe52
Jeigsaw <sumanull@outlook.com>
2023-10-02 12:32:47 +0545

    Add data fetching function using platzi fake store api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,32 +1,52 @@
-import { Button } from "antd";
 import Image from "next/image";
+import { Button } from "antd";
+import { FiEdit3 } from "react-icons/fi";
 
-export default function Home() {
+// mila: use fetch request to fakestoreapi and use it here
+async function getData () {
+  const res = await fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=10');
+  return res.json();
+}
+
+export default async function Home() {
+  const products = await getData();
+
   return (
     <>
-      <h2 className="text-3xl font-bold mb-1"> Welcome to our shop.</h2>
+      <h2 className="text-2xl md:text-3xl font-bold mb-3"> Welcome to our shop.</h2>
       <p>We offer wonderful shopping experience.</p>
 
-      <section className="mt-8">
+      <section className="mt-8 pb-8">
         <h3>New Arrivals (25)</h3>
 
-      {/* Product Gallery */}
+        {/* Product Gallery */}
         <div className="flex flex-row flex-wrap gap-6 mt-4">
-
-          <div className="py-4 px-6 border-[1px] border-[#eee] rounded-[4px] cursor-pointer flex flex-col items-center gap-3 hover:border-[#ccc]">
-            <Image className="max-w-[10rem]" width="160" height="160" src="/favicon.ico"></Image>
+        {products.map((product) => 
+          <div key={product.id} className="group overflow-hidden relative py-4 px-6 border-[1px] border-[#eee] rounded-[4px] cursor-pointer flex flex-col items-center gap-3 hover:border-[#ccc]">
+            <Image className="max-w-[10rem]" priority={true} width="160" height="160" src={product.images[0]} alt={`Product image of ${product.title}`}></Image>
             
             {/* Product Title and price */}
             <div className="text-center">
-              <p>One of kind</p>
-              <p className="text-[#956919]">Rs. 3000</p>
+              <h4>{product.title}</h4>
+              <p className="text-[#956919]">Rs. {product.price}</p>
             </div>
 
-            {/* Action Buttons */}
+            {/* Buy Now Action Buttons */}
             <div>
               <Button type="primary" className="bg-[#cfdadb]">Buy Now</Button>
             </div>
+
+            {/* Admin Action button */}
+            <div className="absolute right-1 top-[-100px] group-hover:translate-y-[105px] group-hover:transition-transform">
+              <button className="rounded-full flex flex-row gap-2 justify-center text-sm bg-[#e4e8e9e8] py-2 px-3 text-slate-700 border-[#aabdbf] border-[1px]">
+                <FiEdit3 />
+                <span className="leading-[1]">Edit</span>
+              </button>
+            </div>
+
           </div>
+        )}
+          
           
         </div>
 
